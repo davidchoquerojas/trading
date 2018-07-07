106754c8dea42f4d02850e44ca8e42343185b6fa
davidchoquerojas <choquerojasnoedavid@gmail.com>
2018-07-07 11:44:05 -0500

    cambios de login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 /**
  * Generated class for the LoginPage page.
@@ -14,15 +15,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
+  public formLogin: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder) {
+    this.formLogin = this.formBuilder.group({
+      email: ['',Validators.compose([Validators.maxLength(100),Validators.pattern('^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$'),Validators.required])],
+      password: ['',Validators.compose([Validators.required,Validators.minLength(10)])]
+    });
   }
 
   ionViewDidLoad() {
+
     console.log('ionViewDidLoad LoginPage');
   }
 
-  signin(){
+  signIn(){
+    console.log(this.formLogin.value);
     console.log("sarita es bella durmiente. xd");
   }
 
